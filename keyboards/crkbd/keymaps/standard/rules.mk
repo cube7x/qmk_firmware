5a881e54051a8f5dcd0cc36317ecb648acfe11f0
cube7x <115941581+cube7x@users.noreply.github.com>
2023-03-06 14:19:13 +0100

    arst

---
+++ keyboards/crkbd/keymaps/standard/rules.mk
@@ -0,0 +1,9 @@
+MOUSEKEY_ENABLE = no    # Mouse keys
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+WPM_ENABLE = yes
+COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
