3b3d33efe0fa20bee73630c636bebc5f966e6d85
cube7x <115941581+cube7x@users.noreply.github.com>
2023-06-08 21:35:07 +0200

    Update rules.mk

--- keyboards/crkbd/keymaps/colemak_ger/rules.mk
+++ keyboards/crkbd/keymaps/colemak_ger/rules.mk
@@ -1,4 +1,6 @@
-MOUSEKEY_ENABLE = no    # Mouse keys
+
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes        # Mouse keys
 RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
 RGB_MATRIX_ENABLE = yes    # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
