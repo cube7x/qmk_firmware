a97236be4828d1cf09a07940d5af663c7d1f0acf
cube7x <115941581+cube7x@users.noreply.github.com>
2024-06-18 21:38:44 +0200

    Completely Redesigned Layers

--- keyboards/bastardkb/tbkmini/keymaps/colemak_ger/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/colemak_ger/keymap.c
@@ -17,24 +17,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 #include <keymap_german.h>
 
-#define LT_BSPC LT(1,KC_BSPC)
-#define LT_SPC LT(2,KC_SPC)
-#define LT_0 LT(2,KC_0)
+#define LT_SPC LT(1,KC_SPC)
+#define LT_DEL LT(2,KC_DEL)
+#define LT_0 LT(3,KC_0)
 #define LT_ENT LT(3,KC_ENT)
 #define LT_ESC LT(5,KC_ESC)
 
-#define LC_DEL LCTL_T(KC_DEL)
-#define LC_TAB LCTL_T(KC_TAB)
-#define LC_T LCTL_T(KC_T)
-#define LC_N LCTL_T(KC_N)
-#define LA_S LALT_T(KC_S)
-#define RA_E RALT_T(KC_E)
-#define LG_R LGUI_T(KC_R)
-#define LG_I LGUI_T(KC_I)
+#define LC_BSPC LCTL_T(KC_BSPC)
+#define LC_ESC LCTL_T(KC_ESC)
+#define LA_ENT LALT_T(KC_ENT)
+#define RA_ADIA RALT_T(DE_ADIA)
 #define LA_ESC LALT(KC_ESC)
 
 void keyboard_pre_init_user(void) {
-  // Set our LED pin as output
+  // Set our LED pin as output88
   setPinOutput(24);
   // Turn the LED off
   // (Due to technical reasons, high is off and low is on)
@@ -45,93 +41,94 @@ enum combos {
   desktop_left,
   desktop_right,
   alttab,
-  fancyzones1,
-  fancyzones2,
-  fancyzones3,
-  fancyzones4,
-  gaming1,
-  gaming2,
 };
 
 const uint16_t PROGMEM arlsft[] = {KC_A, KC_R, KC_LSFT, COMBO_END};
-const uint16_t PROGMEM rslsft[] = {KC_R, KC_S, KC_LSFT, COMBO_END};
 const uint16_t PROGMEM stlsft[] = {KC_S, KC_T, KC_LSFT, COMBO_END};
-const uint16_t PROGMEM ardel[] = {KC_A, KC_R, LC_DEL, COMBO_END};
-const uint16_t PROGMEM rsdel[] = {KC_R, KC_S, LC_DEL, COMBO_END};
-const uint16_t PROGMEM stdel[] = {KC_S, KC_T, LC_DEL, COMBO_END};
-const uint16_t PROGMEM atdel[] = {KC_A, KC_T, LC_DEL, COMBO_END};
-const uint16_t PROGMEM pg[] = {KC_P, KC_G, COMBO_END};
-const uint16_t PROGMEM df[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM rslsft[] = {KC_R, KC_S, KC_LSFT, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
     [desktop_left] = COMBO(arlsft, LCTL(LGUI(KC_LEFT))),
     [desktop_right] = COMBO(stlsft, LCTL(LGUI(KC_RIGHT))),
     [alttab] = COMBO(rslsft, LALT(KC_TAB)),
-    [fancyzones1] = COMBO(ardel, LCTL(KC_F21)),
-    [fancyzones2] = COMBO(rsdel, LCTL(KC_F22)),
-    [fancyzones3] = COMBO(stdel, LCTL(KC_F23)),
-    [fancyzones4] = COMBO(atdel, LCTL(KC_F24)),
-    [gaming1] = COMBO(pg, LCTL(KC_1)),
-    [gaming2] = COMBO(df, LCTL(KC_2)),
 };
 
-const key_override_t ssat_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_SS, DE_AT);
+const key_override_t exqu_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_EXLM, DE_QUES);
+const key_override_t ssampr_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_SS, DE_AMPR);
+const key_override_t circdeg_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_CIRC, DE_DEG);
+const key_override_t quot_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_QUOT, DE_DQUO);
+const key_override_t slsh_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_SLSH, DE_BSLS);
+const key_override_t prn_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_LPRN, DE_RPRN);
+const key_override_t cbr_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_LCBR, DE_RCBR);
+const key_override_t brc_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_LBRC, DE_RBRC);
+const key_override_t abk_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_LABK, DE_RABK);
+const key_override_t layer_key_override = ko_make_basic(MOD_MASK_SHIFT, TO(4), TO(6));
+
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
-    &ssat_key_override,
+    &exqu_key_override,
+    &ssampr_key_override,
+    &circdeg_key_override,
+    &quot_key_override,
+    &slsh_key_override,
+    &prn_key_override,
+    &cbr_key_override,
+    &brc_key_override,
+    &abk_key_override,
     NULL // Null terminate the array of overrides!
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Z, DE_ODIA, DE_UDIA,
+      DE_EXLM,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Z, DE_ODIA, DE_UDIA,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       LC_TAB,    KC_A,    LG_R,    LA_S,    LC_T,    KC_G,                         KC_M,    LC_N,    RA_E,    LG_I,    KC_O, DE_ADIA,
+       KC_TAB,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O, RA_ADIA,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LGUI,    KC_Y,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,   DE_SS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          LT_BSPC, KC_LSFT,  LC_DEL,    KC_LALT,  LT_SPC,  LT_ENT
+                                          LC_BSPC, KC_LSFT,  LT_DEL,    LC_ESC,  LT_SPC,  LA_ENT
                                       //`--------------------------'  `--------------------------'
 
   ),
 
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      DE_ASTR, DE_PLUS,    KC_7,    KC_8,    KC_9, DE_MINS,                      DE_BSLS, DE_SLSH,  DE_EQL, DE_AMPR,  DE_DLR, DE_PERC,
+      DE_CIRC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  DE_EQL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       LC_TAB,    LT_0,    KC_4,    KC_5,    KC_6,  KC_DOT,                      DE_EXLM, DE_LPRN, DE_LCBR, DE_LBRC, DE_DQUO, DE_LABK,
+      DE_LABK, DE_QUOT, DE_LBRC, DE_LCBR, DE_LPRN, DE_SLSH,                      KC_HOME, KC_LEFT,   KC_UP,KC_RIGHT, KC_DOWN,  KC_END,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_ENT,  KC_SPC,    KC_1,    KC_2,    KC_3, KC_COMM,                      DE_QUES, DE_RPRN, DE_RCBR, DE_RABK, DE_QUOT, DE_RBRC,
+      DE_ACUT, DE_PLUS, DE_ASTR, DE_PERC, DE_HASH, DE_PIPE,                      DE_TILD,KC_MS_WH_UP,KC_MS_WH_DOWN, DE_EURO, DE_DLR, TO(4),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          XXXXXXX, XXXXXXX, XXXXXXX,    KC_LCTL,  KC_SPC, KC_LSFT
+                                          LC_BSPC, KC_LSFT,  KC_SPC,    XXXXXXX, XXXXXXX, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   ),
 
-  [2] = LAYOUT_split_3x6_3( 
+  [2] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                      DE_CIRC, KC_HOME,   KC_UP,  KC_END, KC_PGUP,  TO(6),
+       DE_EQL, DE_PLUS,    KC_7,    KC_8,    KC_9, DE_MINS,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       LC_TAB, KC_LALT,   KC_F4,   KC_F5,   KC_F6,  KC_F11,                      DE_HASH, KC_LEFT, KC_DOWN,KC_RIGHT, KC_PGDN,  TO(4),
+       KC_TAB,    LT_0,    KC_4,    KC_5,    KC_6,  KC_DOT,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_ENT,  KC_SPC,   KC_F1,   KC_F2,   KC_F3,  KC_F12,                      DE_TILD, DE_EURO, DE_PIPE,  DE_DEG, DE_ACUT, XXXXXXX,
+       KC_ENT,  KC_SPC,    KC_1,    KC_2,    KC_3, KC_COMM,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_BSPC, KC_LSFT,  LC_DEL,    XXXXXXX, XXXXXXX, XXXXXXX
+                                          XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   ),
 
   [3] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, KC_MS_WH_UP, XXXXXXX, XXXXXXX,                  XXXXXXX, KC_MNXT, KC_MS_UP, KC_MPRV, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_MS_BTN3, KC_MS_BTN2, KC_MS_WH_DOWN, KC_MS_BTN1, XXXXXXX,       KC_MPLY, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, XXXXXXX, KC_SLEP,
+      XXXXXXX, XXXXXXX,   KC_F4,   KC_F5,   KC_F6,  KC_F11,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_MUTE, KC_VOLU, KC_VOLD, KC_CALC, KC_PWR,
+      XXXXXXX, XXXXXXX,   KC_F1,   KC_F2,   KC_F3,  KC_F12,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_BSPC, KC_LSFT,  LC_DEL,    XXXXXXX, XXXXXXX, XXXXXXX
+                                          XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   ),
+
   [4] = LAYOUT_split_3x6_3(
   //Shooter
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
@@ -144,6 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                              KC_C,  KC_SPC, KC_LCTL,      TO(0),    KC_H,  LA_ESC
                                       //`--------------------------'  `--------------------------'
   ),
+
   [5] = LAYOUT_split_3x6_3(
   //Shooter
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
@@ -156,6 +154,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                           XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   ),
+
   [6] = LAYOUT_split_3x6_3(
   //League of Legends
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
