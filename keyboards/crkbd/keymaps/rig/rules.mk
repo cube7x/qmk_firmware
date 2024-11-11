598c43adbe482e9bfc199e2bd277f3d5ca33d4e2
cube7x <115941581+cube7x@users.noreply.github.com>
2024-11-11 17:01:23 +0100

    !

---
+++ keyboards/crkbd/keymaps/rig/rules.mk
@@ -0,0 +1,12 @@
+
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no        # Mouse keys
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+WPM_ENABLE = yes
+COMBO_ENABLE = no
+KEY_OVERRIDE_ENABLE = yes
+CONVERT_TO=promicro_rp2040
