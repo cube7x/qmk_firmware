0848ca4e8495ffce7b1268bfb0c03ee910655eb7
cube7x <115941581+cube7x@users.noreply.github.com>
2026-08-10 00:00:14 +0200

    Add config.h for splinky_2 (RP2040) variant

---
+++ keyboards/bastardkb/tbkmini/v2/splinky_2/config.h
@@ -0,0 +1,7 @@
+// Copyright 2022 Charly Delay <charly@codesink.dev> (@0xcharly)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+/* RGB settings for RP2040 (uses PIO for WS2812). */
+#define WS2812_PIO_USE_PIO1
+#define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_GRB
